1a09941ced3433a1e71a3e50f01d4e819de7dbf8
thomasLevans <tlevans@g.cofc.edu>
2017-09-04 17:09:44 -0700

    #4 Tweets List Roughed

--- src/pages/tweets/tweets.ts
+++ src/pages/tweets/tweets.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Tweets } from '../../data/tweets';
+
 /**
  * Generated class for the TweetsPage page.
  *
@@ -16,9 +18,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class TweetsPage {
 
   title: string;
+  tweets: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.title = 'Tweets';
+    this.tweets = Tweets.statuses;
   }
 
   ionViewDidLoad() {
